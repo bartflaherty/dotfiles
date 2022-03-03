2350a3b684469c317eba1e873362384a2edfec1d
Bart Flaherty <bart.flaherty@discordapp.com>
2022-03-03 17:52:37 +0000

    stop appending powerlevel10k theme to zshrc each time

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-chsh -s $(which zsh)
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
